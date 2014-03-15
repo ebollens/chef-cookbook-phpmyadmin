21c6df6e3d4cefe56d8cd507e53c40f51b3f810f
Eric Bollens <eric@eb.io>
2014-03-15 09:09:09 -0700

    Add config.inc.php creation

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['phpmyadmin']['config']['servers'] = []
+default['phpmyadmin']['config']['blowfish_secret'] = '416398ab66fa23.19220647'
+default['phpmyadmin']['config']['DefaultLang'] = 'en'
+default['phpmyadmin']['config']['ServerDefault'] = 1
+default['phpmyadmin']['config']['UploadDir'] = ''
+default['phpmyadmin']['config']['SaveDir'] = ''
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,6 @@
 package "phpmyadmin"
+
+template "#{node['phpmyadmin']['docroot']}/config.inc.php" do
+  source "config.inc.php.erb"
+  variables({:config => node['phpmyadmin']['config']})
+end
